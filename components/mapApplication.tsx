0a6deb9fd44aee909d38eca2c7c18cfcb65fac5b
Vegard Falldalen <vegfall@gmail.com>
2024-01-17 16:57:32 +0100

    Created a map application component

---
+++ components/mapApplication.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function MapApplication() {
+    return <>
+        <header>
+            <h1>My Map</h1>
+        </header>
+        <nav>
+            Actions
+        </nav>
+        <main>
+            Here is the map
+        </main>
+    </>;
+}
--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {MapApplication} from "./components/mapApplication";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
-root.render(<h1>Hello world</h1>)
+root.render(<MapApplication />)
